14fe7fd5ae9e6953ce63a789d009a31180496373
Victor Jockin <148845139+victorjockin@users.noreply.github.com>
2025-10-06 16:50:19 +0200

    doc css updated

--- docs/assets/js/toc.js
+++ docs/assets/js/toc.js
@@ -55,23 +55,3 @@ if (main) {
   const observer = new MutationObserver(() => generateTOC());
   observer.observe(main, { childList: true, subtree: true });
 }
-
-window.addEventListener("scroll", () => {
-  const headers = document.querySelectorAll("main h2, main h3, main h4");
-  let current = null;
-  const offset = 100; // marge pour activer un peu avant
-
-  headers.forEach(h => {
-    const rect = h.getBoundingClientRect();
-    if (rect.top <= offset && rect.bottom > 0) {
-      current = h;
-    }
-  });
-
-  document.querySelectorAll("#toc button").forEach(btn => {
-    btn.classList.remove("active");
-    if (current && btn.textContent === current.textContent) {
-      btn.classList.add("active");
-    }
-  });
-});
