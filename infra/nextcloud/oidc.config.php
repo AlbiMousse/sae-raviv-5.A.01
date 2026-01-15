b5b51d915ec71822aa4a6fe43b02ff632dfa870d
AlbiMousse <albimousse.game@gmail.com>
2026-01-15 10:51:45 +0100

    [dev] manually merges 17 to dev

---
+++ infra/nextcloud/oidc.config.php
@@ -0,0 +1,11 @@
+<?php
+$CONFIG = array (
+    'oidc_login_client_id' => "nextcloudclient",
+    'oidc_login_client_secret' => "nextcloudsecret",
+    'oidc_login_provider_url' => "https://172.20.10.8/cas/oidc",
+    'oidc_login_logout_url' => "https://172.20.10.8/cas/logout",
+    'oidc_login_auto_redirect' => false,
+    'oidc_login_disable_registration' => false,
+    'oidc_login_tls_verify' => false,
+    'oidc_login_scope' => "openid email profile",
+);
